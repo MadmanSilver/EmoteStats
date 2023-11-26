8a5d91e430fc934c80b25c7fdc9ff0d859855943
MadmanSilver <silverwolf4350@gmail.com>
2023-11-26 01:17:28 -0600

    Added command structure and ping command.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, IntentsBitField, Collection } = require('discord.js');
 
 // Create the client instance.
 const client = new Client({
@@ -12,13 +14,64 @@ const client = new Client({
 });
 
 // Log when the bot successfully connects.
-client.on('ready', (c) => {
+client.once(Events.ClientReady, c => {
     console.log(`${c.user.tag} is online.`);
 });
 
-// Log each message (for testing purposed only)
+// Log each message (for testing purposed only).
 client.on('messageCreate', (msg) => {
     console.log(`(${msg.guild.name}/${msg.channel.name}) ${msg.author.username}: ${msg.content}`);
+});
+
+// Add a collection to the client for easy command access.
+client.commands = new Collection();
+
+// Loop through two layers of folders looking for commands. Register the commands in the collection when found.
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// First folder layer.
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    // Second folder layer.
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+// Listen for slash commands.
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
 
 // Go online
